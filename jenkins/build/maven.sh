416db75bac255f1d00beed24d350423dba56e813
Doruce <gaal.peti2@gmail.com>
2023-08-17 13:52:28 +0000

    fixes

--- jenkins/build/maven.sh
+++ jenkins/build/maven.sh
@@ -2,4 +2,6 @@
 
 echo "Running building jar"
 
-docker run --rm -v $PWD/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.9.3-eclipse-temurin-20-alpine $@
+WORKSPACE=/home/pgaal/jenkins/pipeline/jenkins_home/workspace/maven-docker-pipeline
+
+docker run --rm -v $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3.9.3-eclipse-temurin-20-alpine $@
