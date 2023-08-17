114c85f5dac6101363463375689a8475ba5d03e0
Doruce <gaal.peti2@gmail.com>
2023-08-17 14:36:15 +0000

    fix

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -5,6 +5,6 @@ echo maven_project > /tmp/.auth
 echo $BUILD_TAG  >> /tmp/.auth
 echo $PASS  >> /tmp/.auth
 
-scp -i /opt/prod /tmp/.auth 192.168.157.202:/tmp/.auth
-scp -i /opt/prod ./deploy/publish.sh 192.168.157.202:/tmp/publish.sh
-ssh -i /opt/prod 192.168.157.202 "/tmp/publish.sh"
+scp -i /opt/prod /tmp/.auth pgaal@192.168.157.202:/tmp/.auth
+scp -i /opt/prod ./deploy/publish.sh pgaal@192.168.157.202:/tmp/publish.sh
+ssh -i /opt/prod pgaal@192.168.157.202 "/tmp/publish.sh"
